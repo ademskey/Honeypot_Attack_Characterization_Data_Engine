806655dc5a48671eef427f36e5b68a159f74f035
emilywest1 <emily.west286@gmail.com>
2025-07-16 15:31:41 -0700

    home page

--- web_app/app.py
+++ web_app/app.py
@@ -3,25 +3,31 @@ import query_and_process
 
 app = Flask(__name__)
 
-#df = get_df()
-UPDATE_TIME = 3600 # in seconds
+UPDATE_TIME = 70 # in seconds
 
 # Runs a background thread to run Adam + Caitlyn's pipeline every [update_time] seconds.
 # to provide continuously updated data.
+running_lock = threading.Lock()
 def update_data_loop():
-    global df
     while True:
-        try:
-           query_and_process.main()
-           print("update data loop")
-        except Exception as e:
-            print(f"Error updating DataFrame: {e}")
+        if running_lock.acquire(blocking=False):
+            try:
+                print("Starting a new data update cycle...", flush=True)
+                query_and_process.main()
+                print("Data update cycle completed.", flush=True)
+            except Exception as e:
+                print(f"Data update failed: {e}", flush=True)
+            finally:
+                running_lock.release()
+        else:
+            print("[WARN] Previous data update still running. Skipping this cycle.", flush=True)
+
         time.sleep(UPDATE_TIME)
         
 # Browser Page appearance
-@app.route('/')
+@app.route('/home.html')
 def index():
-    return render_template('hourly.html')
+    return render_template('home.html')
 
 @app.route('/hourly.html')
 def hourly_page():
@@ -73,7 +79,12 @@ def get_tables_in_folder(folder):
                 print(f"Failed to load {file}: {e}")
     return tables
 
-
+# Only start update loop if not in the reloader subprocess (running for first time)
+    # Don't want to interrrupt background thread that updates the dataset.
 if __name__ == '__main__':
-    threading.Thread(target=update_data_loop, daemon=True).start()
+    # Run the background update thread ONLY in the final reloader process
+    # if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+    #     print("Background update thread started")
+    #     threading.Thread(target=update_data_loop, daemon=True).start()
+
     app.run(debug=True)
---
+++ web_app/static/home.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded", () => {
+    window.addEventListener("click", (e) => {
+        if (!e.target.matches(".dropbtn")) {
+            const dropdowns = document.getElementsByClassName("dropdown-content");
+            for (let i = 0; i < dropdowns.length; i++) {
+                dropdowns[i].classList.remove("show");
+            }
+        }
+    });
+});
+
+function toggleDropdown() {
+    document.getElementById("honeypotDropdown").classList.toggle("show");
+}
